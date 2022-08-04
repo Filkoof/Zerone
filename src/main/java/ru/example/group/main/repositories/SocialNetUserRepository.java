839660f53c285a2b4bc1892a1513ff86ef8c571c
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-08-04 18:44:54 +0300

    Security for SocialNetwork26 v1.2 minor fixes, liquibase, entities pulled.

--- src/main/java/ru/example/group/main/repositories/SocialNetUserRepository.java
+++ src/main/java/ru/example/group/main/repositories/SocialNetUserRepository.java
@@ -1,12 +1,11 @@
 package ru.example.group.main.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.example.group.main.entity.UserEntity;
 
 public interface SocialNetUserRepository extends JpaRepository<UserEntity, Integer> {
+    @Query(value = "SELECT * FROM users u WHERE u.e_mail = ?1", nativeQuery = true)
     UserEntity findUserEntityByEMail(String email);
 
-    UserEntity findUserEntityByPhone(String phone);
-
-    UserEntity findUserEntityById(Integer id);
 }
