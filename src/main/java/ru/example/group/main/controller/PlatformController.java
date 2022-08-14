768be4f11fd82ac7e463300b3e8ee5a4798ac675
Vladimir Gorbunov <vairan2411@gmail.com>
2022-08-14 22:03:31 +0300

    language changes

--- src/main/java/ru/example/group/main/controller/PlatformController.java
+++ src/main/java/ru/example/group/main/controller/PlatformController.java
@@ -3,6 +3,7 @@ package ru.example.group.main.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.example.group.main.response.ListResponseLanguageDto;
+import ru.example.group.main.service.PlatformService;
 
 @RestController
 public class PlatformController {
