f6659c8b7c426366a0d81d80449640d95dedd6d2
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-08-18 17:58:36 +0300

    V6.1 Ready for merge to Dev. Dev merged to securityToDev. Email and password change. Tests on H2 liquibase. Security and Registration.

--- src/test/java/ru/example/group/main/repository/UserRoleEntityRepositoryTests.java
+++ src/test/java/ru/example/group/main/repository/UserRoleEntityRepositoryTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import ru.example.group.main.AbstractAllTestH2ContextLoad;
+import ru.example.group.main.entity.UserEntity;
 import ru.example.group.main.entity.UserRoleEntity;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,11 +16,15 @@ class UserRoleEntityRepositoryTests extends AbstractAllTestH2ContextLoad {
 
     @Autowired
     private UserRoleEntityRepository userRoleEntityRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void checkUserRoleEntityRepositorySaveDelete(){
         UserRoleEntity userRoleEntity = new UserRoleEntity();
         userRoleEntity.setUserRole("ROLE_TEST_31232");
+        UserEntity user = userRepository.findAll().get(0);
+        userRoleEntity.setUserForRole(user);
 
         userRoleEntityRepository.save(userRoleEntity);
 
