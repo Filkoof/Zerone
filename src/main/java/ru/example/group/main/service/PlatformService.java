619886f543ef70f306cb17ba400b607835f9bc27
Артем <filkof@mail.ru>
2022-08-23 00:05:14 +0300

    В PostService поправил пагинацию и добавил фильтры на удаленные, заблокированные и посты в очереди

--- src/main/java/ru/example/group/main/service/PlatformService.java
+++ src/main/java/ru/example/group/main/service/PlatformService.java
@@ -5,28 +5,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.example.group.main.dto.LanguageDto;
+import ru.example.group.main.dto.LanguageResponseDto;
 import ru.example.group.main.response.CommonListResponseDto;
 
 @Service
 @Slf4j
 public class PlatformService {
 
-  private static Map<String, LanguageDto> map;
+  private static Map<String, LanguageResponseDto> map;
 
   public PlatformService() {
     map = new HashMap<>();
-    map.put("ru", new LanguageDto(0, "ru"));
-    map.put("eng", new LanguageDto(1, "eng"));
+    map.put("ru", new LanguageResponseDto(0, "ru"));
+    map.put("eng", new LanguageResponseDto(1, "eng"));
   }
 
-  public CommonListResponseDto<LanguageDto> getLanguage() {
-    List<LanguageDto> languages = new ArrayList<>();
+  public CommonListResponseDto<LanguageResponseDto> getLanguage() {
+    List<LanguageResponseDto> languages = new ArrayList<>();
     languages.add(map.get("ru"));
     languages.add(map.get("eng"));
-    return CommonListResponseDto.<LanguageDto>builder()
+    return CommonListResponseDto.<LanguageResponseDto>builder()
             .total(2)
             .perPage(1)
             .offset(0)
