75a24e7a301b1ee4b8e717f500b673f78c412b6c
Джафар Гаибов <xdjafarx@gmail.com>
2022-08-26 17:58:43 +0300

    new dependency and config

--- src/main/java/ru/example/group/main/Main.java
+++ src/main/java/ru/example/group/main/Main.java
@@ -2,6 +2,10 @@ package ru.example.group.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class Main {
