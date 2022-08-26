910a1e470f4b9269cf2bc3da59a8fab39e4582f7
Артем <filkof@mail.ru>
2022-08-26 11:09:40 +0300

    исправил sql запрос в PostRepository на jpql запрос

--- src/main/java/ru/example/group/main/repository/PostRepository.java
+++ src/main/java/ru/example/group/main/repository/PostRepository.java
@@ -10,6 +10,6 @@ import ru.example.group.main.entity.PostEntity;
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
 
-    @Query(value = "SELECT * FROM posts WHERE is_blocked != false AND is_deleted != false", nativeQuery = true)
+    @Query("SELECT pe FROM PostEntity pe WHERE pe.isBlocked != false AND pe.isDeleted != false")
     Page<PostEntity> findAllPostsWithPagination(String text, Pageable pageable);
 }
