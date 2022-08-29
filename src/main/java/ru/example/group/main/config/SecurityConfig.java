92649792da18031fc5e403183f0f59d081e70106
Джафар Гаибов <xdjafarx@gmail.com>
2022-08-29 18:59:13 +0300

    SecurityConfig modified, application.yml modified, SpringFoxConfig.java deleted

--- src/main/java/ru/example/group/main/config/SecurityConfig.java
+++ src/main/java/ru/example/group/main/config/SecurityConfig.java
@@ -13,6 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.example.group.main.security.JWTRequestFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
 @Configuration
@@ -37,10 +41,22 @@ public class SecurityConfig {
                 .antMatchers("/api/v1/auth/*","/auth/api/logout","/api/v1/account/register", "/api/v1/account/recovery", "/api/v1/account/register/confirm").permitAll()
                 .antMatchers("/api/v1/account/registration_complete/*", "/api/v1/account/recovery_complete", "/api/v1/support").permitAll()
                 .antMatchers("/api/v1/platform/languages", "/email_change/confirm", "/password_change/confirm", "/api/v1/account/password/set").permitAll()
+                .antMatchers("/swagger-resources/*", "*.html", "/api/v1/swagger.json", "/swagger-ui").permitAll()
                 .antMatchers( "**").authenticated()
                 .and().formLogin();
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Configuration
+    public class SpringFoxConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
     }
 
 }
