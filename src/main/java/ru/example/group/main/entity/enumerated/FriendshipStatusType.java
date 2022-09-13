ab03451b4cb878c9e9b1759b43e3f7acd80abab6
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-09-13 17:12:10 +0300

    Friends recommendations v5.1 minor cleanup.

--- src/main/java/ru/example/group/main/entity/enumerated/FriendshipStatusType.java
+++ src/main/java/ru/example/group/main/entity/enumerated/FriendshipStatusType.java
@@ -41,37 +41,6 @@ public enum FriendshipStatusType {
         return result;
     }
 
-    public static Long getFriendshipFromName(String friendshipStatusId) {
-        Long result;
-        switch (friendshipStatusId) {
-            case "REQUEST":
-                result = 1L;
-                break;
-            case "FRIEND":
-                result = 2L;
-                break;
-            case "BLOCKED":
-                result = 3L;
-                break;
-            case "DECLINED":
-                result = 4L;
-                break;
-            case "SUBSCRIBED":
-                result = 5L;
-                break;
-            case "WASBLOCKEDBY":
-                result = 6L;
-                break;
-            case "DEADLOCK":
-                result = 7L;
-                break;
-            default:
-                result = null;
-                break;
-        };
-        return result;
-    }
-
     public static FriendshipStatusType getFriendshipFromString(String code) {
         FriendshipStatusType result;
         switch (code) {
