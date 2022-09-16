38695f18fb05a598764e8b8888b6632ff116b7f5
Павел Ермишин <ermishinpavel@gmail.com>
2022-09-16 13:23:46 +0300

    fix jooq connection bo bd postgres

--- src/main/java/ru/example/group/main/Main.java
+++ src/main/java/ru/example/group/main/Main.java
@@ -1,6 +1,5 @@
 package ru.example.group.main;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
