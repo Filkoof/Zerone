c18c5e50ba92555f4013d424e77feb1ba4ac2343
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-09-20 10:52:42 +0300

    Swagger And Validation of controllers 2.1.

--- src/main/java/ru/example/group/main/config/SwaggerConfig.java
+++ src/main/java/ru/example/group/main/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package ru.example.group.main.config;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -14,31 +15,35 @@ import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfig {
-        @Bean
-        public Docket api() {
-            return new Docket(DocumentationType.SWAGGER_2)
-                    .select()
-                    .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-                    .paths(PathSelectors.any())
-                    .build()
-                    .apiInfo(apiInfo());
-        }
+
+    @Value("${config.frontend}")
+    private String frontend;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
 
     public ApiInfo apiInfo() {
         return new ApiInfo(
                 "Zerone API",
                 """
                         API for Zerone social network project.
-                        
+                                                
                         Please, for secured APIs!
                         RECEIVE TOKEN FIRST USING correct login and password with login API and put it into header name Authorization to get access to secured APIs.
                                                 
                         """,
                 "1.0",
-                "http://195.161.62.32/terms_of_service",
-                new Contact("API owner", "http://195.161.62.32/owner ", "owner@rmailer.org"),
+                "http://" + frontend,
+                new Contact("API owner", "http://" + frontend, "ownerskiyman@ringmailering.rog"),
                 "Zerone_api_license",
-                "http://195.161.62.32/licence",
+                "http://" + frontend,
                 new ArrayList<>()
         );
     }
