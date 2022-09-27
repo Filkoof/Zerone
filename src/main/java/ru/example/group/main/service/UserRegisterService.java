55e4cf25659c48ffc712dea8efbec45e16e99925
Павел Ермишин <ermishinpavel@gmail.com>
2022-09-27 22:26:38 +0300

    Merge remote-tracking branch 'origin/#53_city_reg_definition' into dev
    # Conflicts:
    #	src/main/java/ru/example/group/main/config/SecurityConfig.java
    #	src/main/java/ru/example/group/main/controller/UserRegisterController.java
    #	src/main/java/ru/example/group/main/controller/UserSettingsController.java
    #	src/main/java/ru/example/group/main/service/SearchService.java
    #	src/main/java/ru/example/group/main/service/UserRegisterService.java
    #	src/main/java/ru/example/group/main/service/UserSettingsService.java

--- src/main/java/ru/example/group/main/service/UserRegisterService.java
+++ src/main/java/ru/example/group/main/service/UserRegisterService.java
@@ -57,7 +57,7 @@ public class UserRegisterService {
 
     private final RecommendedFriendsService recommendedFriendsService;
 
-    private final static String localFileGeoLite2 = "static/GeoLite2-City.mmdb";
+    private final static String localFileGeoLite2 = "GeoLite2-City.mmdb";
 
     private final static String remoteFileGeoLite2 = "https://git.io/GeoLite2-City.mmdb";
 
@@ -107,7 +107,7 @@ public class UserRegisterService {
 
     public RegistrationCompleteResponseDto activateUser(RegisterConfirmRequestDto registerConfirmRequestDto,
                                                         HttpServletRequest request) throws NewUserConfirmationViaEmailFailedException, IOException, URISyntaxException, GeoIp2Exception, NoSuchAlgorithmException, InterruptedException {
-       // List<String> cityResponse = getLocationFromUserIp(getClientIp(request));
+        List<String> cityResponse = getLocationFromUserIp(getClientIp(request));
         UserEntity user = userRepository.findByConfirmationCode(registerConfirmRequestDto.getToken());
         RegistrationCompleteResponseDto registrationCompleteResponseDto = new RegistrationCompleteResponseDto();
         if (user == null || !user.getEmail().equals(registerConfirmRequestDto.getUserId())) {
@@ -116,8 +116,8 @@ public class UserRegisterService {
         try {
             user.setConfirmationCode(null);
             user.setApproved(true);
-          //  user.setCountry(cityResponse.get(0));
-          //  user.setCity(cityResponse.get(1));
+            user.setCountry(cityResponse.get(0));
+            user.setCity(cityResponse.get(1));
             userRepository.save(user);
             recommendedFriendsService.runNewUserActivatedFriendsRecommendationsUpdate(user.getId());
             registrationCompleteResponseDto.setEMail(user.getEmail());
