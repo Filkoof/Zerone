58dcb2671faf94b4420a68cb4a9f9606edc49239
Павел Ермишин <ermishinpavel@gmail.com>
2022-09-27 17:37:29 +0300

    Merge remote-tracking branch 'origin/#53_city_reg_definition' into dev
    # Conflicts:
    #	src/main/java/ru/example/group/main/config/SecurityConfig.java
    #	src/main/java/ru/example/group/main/controller/UserRegisterController.java
    #	src/main/java/ru/example/group/main/controller/UserSettingsController.java
    #	src/main/java/ru/example/group/main/service/SearchService.java
    #	src/main/java/ru/example/group/main/service/UserRegisterService.java
    #	src/main/java/ru/example/group/main/service/UserSettingsService.java

--- src/main/java/ru/example/group/main/service/UserRegisterService.java
+++ src/main/java/ru/example/group/main/service/UserRegisterService.java
@@ -105,7 +105,7 @@ public class UserRegisterService {
 
     public RegistrationCompleteResponseDto activateUser(RegisterConfirmRequestDto registerConfirmRequestDto,
                                                         HttpServletRequest request) throws NewUserConfirmationViaEmailFailedException, IOException, URISyntaxException, GeoIp2Exception, NoSuchAlgorithmException, InterruptedException {
-        List<String> cityResponse = getLocationFromUserIp(getClientIp(request));
+       // List<String> cityResponse = getLocationFromUserIp(getClientIp(request));
         UserEntity user = userRepository.findByConfirmationCode(registerConfirmRequestDto.getToken());
         RegistrationCompleteResponseDto registrationCompleteResponseDto = new RegistrationCompleteResponseDto();
         if (user == null || !user.getEmail().equals(registerConfirmRequestDto.getUserId())) {
@@ -114,8 +114,8 @@ public class UserRegisterService {
         try {
             user.setConfirmationCode(null);
             user.setApproved(true);
-            user.setCountry(cityResponse.get(0));
-            user.setCity(cityResponse.get(1));
+          //  user.setCountry(cityResponse.get(0));
+          //  user.setCity(cityResponse.get(1));
             userRepository.save(user);
             recommendedFriendsService.runNewUserActivatedFriendsRecommendationsUpdate(user.getId());
             registrationCompleteResponseDto.setEMail(user.getEmail());
@@ -160,11 +160,11 @@ public class UserRegisterService {
             return location;
         } else {
             URL resource = getClass().getClassLoader().getResource(localFileGeoLite2);
-            if (resource == null) {
+            if (resource == null || localFileSizeEquallyRemoteFileSize()) {
                 downLoadGeoLite();
             }
             URL resourceUpdated = getClass().getClassLoader().getResource(localFileGeoLite2);
-            database = new File(localFileGeoLite2);
+            database = new File(resourceUpdated.toURI());
         }
         DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
         InetAddress addr = InetAddress.getByName(ipAddress);
