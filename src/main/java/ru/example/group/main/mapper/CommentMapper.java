bd08c4f8f801a8245519b425a92acbd2aa1658cd
Артем <filkof@mail.ru>
2022-10-12 13:33:38 +0300

    Отрефакторил код, для лушчего понимаия и дальнейшей работы

--- src/main/java/ru/example/group/main/mapper/CommentMapper.java
+++ src/main/java/ru/example/group/main/mapper/CommentMapper.java
@@ -14,17 +14,27 @@ import ru.example.group.main.entity.UserEntity;
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface CommentMapper {
 
-
     @Mapping(target = "myLike", ignore = true, defaultValue = "false")
     @Mapping(target = "likes", ignore = true)
     @Mapping(target = "images", ignore = true)
-    @Mapping(target = "commentText", expression = "java(entity.isDeleted()?\"коммент удален\":entity.getCommentText())")
+    @Mapping(target = "commentText", expression = "java(entity.isDeleted() ? \"коммент удален\" : entity.getCommentText())")
     @Mapping(target = "author", source = "user")
     @Mapping(target = "postId", source = "post.id")
     @Mapping(target = "parentId", source = "parent.id")
     @Mapping(target = "blocked", source = "blocked")
     @Mapping(target = "deleted", source = "deleted")
     CommentDto commentEntityToDto(CommentEntity entity);
+
+    @Mapping(target = "myLike", ignore = true, defaultValue = "false")
+    @Mapping(target = "likes", ignore = true)
+    @Mapping(target = "images", expression = "java(dto.getImageDtoList())")
+    @Mapping(target = "commentText", expression = "java(entity.isDeleted() ? \"коммент удален\" : entity.getCommentText())")
+    @Mapping(target = "author", source = "entity.user")
+    @Mapping(target = "postId", source = "entity.post.id")
+    @Mapping(target = "parentId", source = "entity.parent.id")
+    @Mapping(target = "blocked", source = "entity.blocked")
+    @Mapping(target = "deleted", source = "entity.deleted")
+    CommentDto commentEntityImageToDto(CommentEntity entity, CommentRequestDto dto);
 
     @Mapping(target = "subComments", ignore = true)
     @Mapping(target = "id", ignore = true)
@@ -34,12 +44,8 @@ public interface CommentMapper {
     @Mapping(target = "post", source = "postEntity")
     @Mapping(target = "user", source = "user")
     @Mapping(target = "time", ignore = true)
-    @Mapping(target = "commentText", source = "dto.commentText")
-    CommentEntity commentRequestDtoToEntity(
-            CommentRequestDto dto,
-            PostEntity postEntity,
-            UserEntity user,
-            CommentEntity parentComment);
+    @Mapping(target = "commentText", expression = "java(dto.getCommentText())")
+    CommentEntity commentRequestDtoToEntity(CommentRequestDto dto, PostEntity postEntity, UserEntity user, CommentEntity parentComment);
 
     List<CommentDto> commentListDto(Page<CommentEntity> list);
 }
--- src/main/java/ru/example/group/main/service/CommentService.java
+++ src/main/java/ru/example/group/main/service/CommentService.java
@@ -3,6 +3,7 @@ package ru.example.group.main.service;
 import java.time.LocalDateTime;
 import javax.persistence.EntityNotFoundException;
 
+import io.jsonwebtoken.lang.Assert;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +17,7 @@ import ru.example.group.main.entity.CommentEntity;
 import ru.example.group.main.exception.CommentPostNotFoundException;
 import ru.example.group.main.exception.IdUserException;
 import ru.example.group.main.mapper.CommentMapper;
+import ru.example.group.main.mapper.UserMapper;
 import ru.example.group.main.repository.CommentRepository;
 import ru.example.group.main.repository.PostRepository;
 import ru.example.group.main.security.SocialNetUserRegisterService;
@@ -28,17 +30,34 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
     private final SocialNetUserRegisterService socialNetUserRegisterService;
-    private final CommentMapper mapper;
+    private final CommentMapper commentMapper;
 
-    public CommonListResponseDto<CommentDto> getCommentsForPostId(Long idPost, int offset, int itemPerPage) {
-        if (postRepository.existsById(idPost)) {
-            return getCommonList(idPost, itemPerPage, offset);
-        } else throw new EntityNotFoundException();
+    public CommonResponseDto<CommentDto> postComment(Long postId, CommentRequestDto request) {
+        Assert.notNull(postId, "id поста не может быть null");
+
+        var currentUser = socialNetUserRegisterService.getCurrentUser();
+        var postEntity = postRepository.findById(postId).orElseThrow(EntityNotFoundException::new);
+        var parentComment = request.getParentId() != null ? commentRepository.getReferenceById(request.getParentId()) : null;
+
+        var commentEntity = commentMapper.commentRequestDtoToEntity(request, postEntity, currentUser, parentComment);
+        commentRepository.save(commentEntity);
+
+        return getCommonResponseDto(commentEntity);
     }
 
-    public ResponseEntity<CommonResponseDto<CommentDto>> postCommentForPostId(Long id, CommentRequestDto request) {
-        var comment = commentRepository.save(getCommentEntity(id, request));
-        return ResponseEntity.ok(getCommonResponseDto(comment));
+    public CommonListResponseDto<CommentDto> getComments(Long postId, int offset, int itemPerPage) {
+        Assert.notNull(postId, "id поста не может быть null");
+
+        var pageable = PageRequest.of(offset / itemPerPage, itemPerPage);
+        var commentEntityPage = commentRepository.findByCommentToPost(postId, pageable);
+
+        return CommonListResponseDto.<CommentDto>builder()
+                .total((int) commentEntityPage.getTotalElements())
+                .data(commentEntityPage.stream().map(commentMapper::commentEntityToDto).toList())
+                .perPage(itemPerPage)
+                .error("")
+                .timestamp(LocalDateTime.now())
+                .offset(offset).build();
     }
 
     public ResponseEntity<CommonResponseDto<CommentDto>> deleteComment(long idPost, long comment_id)
@@ -72,6 +91,14 @@ public class CommentService {
         return ResponseEntity.ok(getCommonResponseDto(comment));
     }
 
+    private CommentEntity getCommentFromRequest(CommentEntity commentEntity, CommentRequestDto request) {
+        commentEntity.setCommentText(request.getCommentText());
+        if (request.getParentId() != null) {
+            commentEntity.setParent(commentRepository.getReferenceById(request.getParentId()));
+        }
+        return commentEntity;
+    }
+
     public ResponseEntity<CommonResponseDto<CommentDto>> recoverComment(long idPost, long comment_id)
             throws EntityNotFoundException, IdUserException, CommentPostNotFoundException {
         var user = socialNetUserRegisterService.getCurrentUser();
@@ -88,38 +115,24 @@ public class CommentService {
         return ResponseEntity.ok(getCommonResponseDto(comment));
     }
 
-    private CommonResponseDto<CommentDto> getCommonResponseDto(CommentEntity comment) {
-        var response = new CommonResponseDto<CommentDto>();
-        response.setError("");
-        response.setTimeStamp(LocalDateTime.now());
-        response.setData(mapper.commentEntityToDto(comment));
-        return response;
-    }
-
-    private CommentEntity getCommentEntity(Long postId, CommentRequestDto request) {
-        var post = postRepository.getReferenceById(postId);
-        var user = socialNetUserRegisterService.getCurrentUser();
-        var parent = request.getParentId() != null ? commentRepository.getReferenceById(request.getParentId()) : null;
-        return mapper.commentRequestDtoToEntity(request, post, user, parent);
-    }
-
-    private CommentEntity getCommentFromRequest(CommentEntity commentEntity, CommentRequestDto request) {
-        commentEntity.setCommentText(request.getCommentText());
-        if (request.getParentId() != null) {
-            commentEntity.setParent(commentRepository.getReferenceById(request.getParentId()));
-        }
-        return commentEntity;
-    }
-
     public CommonListResponseDto<CommentDto> getCommonList(Long idPost, int itemPerPage, int offset) {
         var pageable = PageRequest.of(offset / itemPerPage, itemPerPage);
-        var listCommentEntity = commentRepository.findByCommentToPost(idPost, pageable);
+        var commentEntityPage = commentRepository.findByCommentToPost(idPost, pageable);
         return CommonListResponseDto.<CommentDto>builder()
                 .perPage(itemPerPage)
-                .total((int) listCommentEntity.getTotalElements())
+                .total((int) commentEntityPage.getTotalElements())
                 .error("")
                 .timestamp(LocalDateTime.now())
-                .data(mapper.commentListDto(listCommentEntity))
-                .offset(offset).build();
+                .data(commentEntityPage.stream().map(commentMapper::commentEntityToDto).toList())
+                .offset(offset)
+                .build();
+    }
+
+    private CommonResponseDto<CommentDto> getCommonResponseDto(CommentEntity comment) {
+        return CommonResponseDto.<CommentDto>builder()
+                .data(commentMapper.commentEntityToDto(comment))
+                .error("")
+                .timeStamp(LocalDateTime.now())
+                .build();
     }
 }
