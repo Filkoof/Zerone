dfedf9bd79ab3ff96ac32b60d5c0b808102aab62
Артем <filkof@mail.ru>
2022-10-24 18:08:29 +0300

    editPost почти готово

--- src/main/java/ru/example/group/main/controller/PostController.java
+++ src/main/java/ru/example/group/main/controller/PostController.java
@@ -3,7 +3,6 @@ package ru.example.group.main.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.example.group.main.dto.request.PostRequestDto;
@@ -21,15 +20,6 @@ import javax.persistence.EntityNotFoundException;
 @Api("News(posts) api")
 public class PostController {
     private final PostService postService;
-
-    @GetMapping("/feeds")
-    @ApiOperation("Operation to get News(posts) feed.")
-    public CommonListResponseDto<PostResponseDto> getNewsfeed(
-            @RequestParam(name = "offset", defaultValue = "0") int offset,
-            @RequestParam(name = "itemPerPage", defaultValue = "20") int itemPerPage
-    ) throws PostsException {
-        return postService.getNewsfeed(offset, itemPerPage);
-    }
 
     @PostMapping("/users/{id}/wall")
     @ApiOperation("Operation to add new post for user id (@PathVariable).")
@@ -49,12 +39,10 @@ public class PostController {
         return postService.getNewsUserId(id, offset);
     }
 
-    @GetMapping("/post/{id}")
-    @ApiOperation("Operation to get post by post_id (@PathVariable).")
-    public ResponseEntity<CommonResponseDto<PostResponseDto>> getPostById(@PathVariable long id)
-            throws EntityNotFoundException, PostsException {
-        //return postService.getPost(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PutMapping("/post/{id}/recover")
+    @ApiOperation("Operation to recover post with id (@PathVariable).")
+    public ResponseEntity<PostResponseDto> postRecover(@PathVariable Long id) throws PostsException {
+        return postService.recoverPost(id);
     }
 
     @DeleteMapping("/post/{id}")
@@ -64,9 +52,20 @@ public class PostController {
         return postService.deletePost(id);
     }
 
-    @PutMapping("/post/{id}/recover")
-    @ApiOperation("Operation to recover post with id (@PathVariable).")
-    public ResponseEntity<PostResponseDto> postRecover(@PathVariable Long id) throws PostsException {
-        return postService.recoverPost(id);
+    @PutMapping("/api/v1/post/{id}")
+    public CommonResponseDto<PostResponseDto> editPost(
+            @PathVariable long id,
+            @RequestParam(name = "publish_date", defaultValue = "0") long publishDate,
+            @RequestBody PostRequestDto request) throws PostsException {
+        return postService.editPost(id, publishDate, request);
+    }
+
+    @GetMapping("/feeds")
+    @ApiOperation("Operation to get News(posts) feed.")
+    public CommonListResponseDto<PostResponseDto> getNewsfeed(
+            @RequestParam(name = "offset", defaultValue = "0") int offset,
+            @RequestParam(name = "itemPerPage", defaultValue = "20") int itemPerPage
+    ) throws PostsException {
+        return postService.getNewsfeed(offset, itemPerPage);
     }
 }
